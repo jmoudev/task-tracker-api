65191d3c5858fa16d3f49300f8bab1b7e450f2e3
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2024-12-23 23:52:12 +0000

    feat: adds endpoint to update todos

--- main.py
+++ main.py
@@ -36,3 +36,14 @@ def create_todo(todo: ToDo):
     todo_data["metadata"]["todo_count"] += 1
     utils.write_json(TODO_JSON_FILENAME, todo_data)
     return todo_list_item
+
+
+@app.put("/todos/{todo_id}", response_model=ToDoListItem)
+def update_todo(todo_id: int, todo: ToDo):
+    todo_data = utils.read_todo_data(TODO_JSON_FILENAME)
+    if not todo_data["todos"].get(str(todo_id)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"todo not found (id {todo_id})")
+    todo_list_item = {"id": todo_id} | todo.dict()
+    todo_data["todos"][str(todo_id)] = todo_list_item
+    utils.write_json(TODO_JSON_FILENAME, todo_data)
+    return todo_list_item
