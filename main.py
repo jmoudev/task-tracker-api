2d99210edfe81426a62f0694c2ce277603b70363
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2024-12-24 01:05:08 +0000

    feat: adds endpoint to get paginated todos

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
+from typing import Annotated, Optional
+
+from fastapi import FastAPI, HTTPException, Query, status
+from pydantic import BaseModel, Field
 
 import utils
 
@@ -57,3 +59,35 @@ def delete_todo(todo_id: int):
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"todo not found (id {todo_id})")
     utils.write_json(TODO_JSON_FILENAME, todo_data)
+
+
+class PaginationQuery(BaseModel):
+    page: Optional[int] = 1
+    limit: Optional[int] = 10
+
+
+class PaginatedToDoView(BaseModel):
+    data: list[ToDoListItem]
+    page: int = Field(gt=0)
+    limit: int = Field(gt=0,lte=20)
+    total: int = Field(gte=0,lte=20)
+
+
+@app.get("/todos/", response_model=PaginatedToDoView)
+def get_todos(pagination_query: Annotated[PaginationQuery, Query()]):
+    todo_data = utils.read_todo_data(TODO_JSON_FILENAME)
+    limit = pagination_query.limit
+    todo_list = list(todo_data["todos"].values())
+    paginated_todo_list = [todo_list[i:i+limit] for i in range(0, len(todo_list), limit)]
+    page = pagination_query.page
+    try:
+        todo_list_page = paginated_todo_list[page-1]
+    except IndexError:
+        todo_list_page = []
+    paginated_todo_view = {
+        "data": todo_list_page,
+        "page": pagination_query.page,
+        "limit": limit,
+        "total": len(todo_list_page)
+    }
+    return paginated_todo_view
